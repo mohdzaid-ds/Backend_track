bdd2bef358abc0b8f97b0bf12fd63e0b9e89bd2a
mohd_zaid <mz495827@gmail.com>
2026-09-05 02:12:54 -0700

    Stage 2:  Read Endpoints with 404

---
+++ CRUD_API/read_task.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Build CRUD API",
+        "done": False
+    },
+    {
+        "id": 3,
+        "title": "Complete FlyRank Assignment",
+        "done": False
+    }
+]
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {task_id} not found"}
+    )
